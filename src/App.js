4d645a6bbc0c3185763b79efdf01aff909d838f0
Vignika <20eg112132@anurag.edu.in>
2024-08-16 11:15:41 +0530

    Board with squares

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Square from './components/Square';
+import './styles.css';
+import Board from './components/Board';
+
 
 function App() {
   return (
     <div className="App">
-      <Square/>
+    <Board/>
     </div>
   );
 }
